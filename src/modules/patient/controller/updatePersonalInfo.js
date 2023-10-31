a3da03ac4d40c7c70817ab32dbd2078c0e5fa8d0
yousef <yousefmahmoudmahmed@outlook.com>
2023-10-31 23:57:21 +0200

    create updatePersonalInfo controller

---
+++ src/modules/patient/controller/updatePersonalInfo.js
@@ -0,0 +1,12 @@
+import patientModel from "../../../../DB/models/Patient.model.js";
+import doctorModel from "../../../../DB/models/doctor.model.js";
+import { asyncErrorHandler } from "../../../utils/errorHandling.js";
+import updatePersonalInfo from "../../../utils/handlers/updatePersonalInfo.js";
+
+const updatePersonalInfoController = asyncErrorHandler((req, res, next) => {
+  if (req.originalUrl.includes("patient"))
+    updatePersonalInfo(patientModel)(req, res, next);
+  else updatePersonalInfo(doctorModel)(req, res, next);
+});
+
+export default updatePersonalInfoController;
