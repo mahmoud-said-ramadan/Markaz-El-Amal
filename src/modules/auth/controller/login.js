2e72555b700782db5d7c4521e0be90359a7b3db7
NadaAmrr <nada.amr1267@gmail.com>
2023-10-31 23:42:15 +0200

    login

---
+++ src/modules/auth/controller/login.js
@@ -0,0 +1,29 @@
+import { StatusCodes } from "http-status-codes";
+import { compare } from "../../../utils/HashAndCompare.js";
+import ErrorClass from "../../../utils/errorClass.js";
+import { generateToken } from "../../../utils/GenerateAndVerifyToken.js";
+import { role } from "../../../utils/shared.js";
+import { allMessages } from "../../../utils/localizationHelper.js";
+
+export const login = async (req, res, next) => {
+    const { email, password } = req.body;
+    const model = role(req.originalUrl)
+    const user = await model.findOne({ email });
+    if (!user) {
+      return next(new ErrorClass(allMessages[req.query.ln].INVALID_INFO, StatusCodes.NOT_FOUND));
+    }
+    // Check confirm email
+    if (!user.confirmed) {
+      return next(new ErrorClass("You have to confirm your Email", StatusCodes.BAD_REQUEST));
+    }
+    // Check password
+    const match = compare({ plaintext: password, hashValue: user.password });
+    if (!match) {
+      return next(new ErrorClass(allMessages[req.query.ln].INVALID_INFO, StatusCodes.BAD_REQUEST));
+    }
+    const accessToken = generateToken({payload: {id: user._id , email:user.email} } )
+    const refreshToken = generateToken({payload: {id: user._id , email:user.email} } , {expiresIn : 60 * 60 * 24 * 30 * 2 } )
+    user.loggedIn = true
+    await user.save() 
+    return res.status(StatusCodes.OK).json({ message: "Done", accessToken, refreshToken });
+  };
