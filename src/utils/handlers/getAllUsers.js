51c53d8f1da2fc17615324f07f68443425dd37b8
yousef <yousefmahmoudmahmed@outlook.com>
2023-10-31 23:53:57 +0200

    create get all users function

---
+++ src/utils/handlers/getAllUsers.js
@@ -0,0 +1,27 @@
+import { StatusCodes } from "http-status-codes";
+import ErrorClass from "../errorClass.js";
+import { allMessages } from "../localizationHelper.js";
+/*
+Needed Data => No data
+Return Data => Message , Users
+*/
+
+const getAllUsers = (model) => {
+  async (req, res, next) => {
+    const users = await model.find(); // Search for  all users in DB
+
+    // Validate that there is users exist in DB
+    if (!users.length) {
+      return next(
+        new ErrorClass(allMessages[req.query.ln].NO_USER_FOUND),
+        StatusCodes.NOT_FOUND
+      );
+    }
+
+    // Return users Data
+    return res
+      .status(StatusCodes.ACCEPTED)
+      .json({ message: allMessages[req.query.ln].SUCCESS, users });
+  };
+};
+export default getAllUsers;
