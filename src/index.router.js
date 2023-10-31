8a62c4a4b2c8763efae61e292ff87f71a8a94f0d
yousef <yousefmahmoudmahmed@outlook.com>
2023-10-31 23:31:49 +0200

    add patient router to index.router

--- src/index.router.js
+++ src/index.router.js
@@ -2,13 +2,13 @@ import { dbConnection } from "../DB/dbConnection.js";
 import ErrorClass from "./utils/errorClass.js";
 import { errorHandel } from "./utils/errorHandling.js";
 import { allMessages } from "./utils/localizationHelper.js";
-
+import patientRouter from "./modules/patient/patient.router.js";
 const initApp = (app, express) => {
   //convert Buffer Data
   app.use(express.json({}));
 
   //*Setup API Routing
-
+  app.use("/patient", patientRouter);
   app.all("*", (req, res, next) => {
     return next(new ErrorClass(allMessages.en.IN_VALID_URL));
   });
