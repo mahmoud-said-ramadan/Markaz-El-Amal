d9f8f87384a4e0a4e395578d5a85d6c9e39ad279
NadaAmrr <nada.amr1267@gmail.com>
2023-11-01 15:39:02 +0200

    last update shared.js

--- src/utils/shared.js
+++ src/utils/shared.js
@@ -3,25 +3,13 @@ import patientModel from "../../DB/models/Patient.model.js";
 import { customAlphabet } from 'nanoid'
 
 export const role = (url) =>{
-  switch (getRolesFromUrl(url)) {
-    case "doctor":
-      return doctorModel;
-    case "patient":
-      return patientModel;
-    case "admin":
-     return doctorModel;
+  if (url.includes("doctor")) {
+    return doctorModel;
+  } else if(url.includes("patient")){
+    return patientModel;
+  } else if(url.includes("admin")){
+    return doctorModel;
   }
 }
-
-export const getRolesFromUrl = (url) => {
-  if (url.includes("doctor")) {
-    return "doctor";
-  } else if (url.includes("patient")) {
-    return "patient";
-  }
-  //  else if (url.includes("admin")) {
-  //   return "admin";
-  // } 
-};
 
 export const code = customAlphabet("0123456789", 5)
