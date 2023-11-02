aec8eaf19d9f33cb899e613b739e8f968bee6cec
NadaAmrr <nada.amr1267@gmail.com>
2023-11-02 20:24:44 +0200

    update patient model

--- DB/models/Patient.model.js
+++ DB/models/Patient.model.js
@@ -1,43 +1,19 @@
-import mongoose , { Schema , model } from "mongoose";
-import { imageSchema } from "../DB_Utils/imageSchema.js";
-import { codeSchema } from "../DB_Utils/codeSchema.js";
+import mongoose , { Schema, model } from "mongoose";
+import { personSchema } from "../DB_Utils/personSchema.js";
 
 const patientSchema = new Schema(
   {
-    name: {
-      type: String,
-      required: true,
-      minLength: 2,
-      lowercase: true,
-    },
-    email: {
-      type: String,
-      unique: true,
-      required: true,
-      lowercase: true,
-    },
-    password: { type: String, required: true },
-    phone: { type: String },
-    loggedIn: { type: Boolean, default: false },
-    confirmed: { type: Boolean, default: false },
-    gender: {
-      type: String,
-      enum: ['male', 'female'],
-      required: false
-    },
-    image: imageSchema,
-    OTP: codeSchema,
-    customId: String,
-    DateOfBirth: { type: Date },
-    //------------ end shared
-      history: [{
+    ...personSchema,
+    history: [
+      {
         type: String,
         date: {
-            type: Date,
-            required: true
-          },
-          required: false,
-      }]
+          type: Date,
+          required: true,
+        },
+        required: false,
+      },
+    ],
   },
   {
     timestamps: true,
