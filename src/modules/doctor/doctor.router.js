3f351f251df1dcd49f5a367ed63be8a342b3642e
yousef <yousefmahmoudmahmed@outlook.com>
2023-11-03 00:57:47 +0200

    fixed a bug

--- src/modules/doctor/doctor.router.js
+++ src/modules/doctor/doctor.router.js
@@ -1,8 +1,6 @@
 import Router from "express";
-import addDate from "./controller/addDates.js";
 import { auth } from "../../middleware/auth.js";
 import doctorEndpoint from "./doctor.endpoint.js";
-import removeDate from "./controller/removeDates.js";
 const router = Router();
 
 export default router;
