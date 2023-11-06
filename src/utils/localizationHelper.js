79429484ec101407e36146506907b4b709964bf5
yousef <yousefmahmoudmahmed@outlook.com>
2023-11-06 20:04:02 +0200

    modify NO_REVIEW_FOUND message

--- src/utils/localizationHelper.js
+++ src/utils/localizationHelper.js
@@ -84,7 +84,7 @@ export const allMessages =
         CODE_DURATION_NOT_ENDED:"You Can NOT Ask For a new Code, Until 5 Mints from The Last Sended One!",
         EMAIL_NOT_FOUND:"NOT REGISTERED!... Please signUp!",
         EMAIL_SAME_AS_OLD:"This is your old email, You must choose different email",
-        NO_REVIEW_FOUND:"There's no review for this doctor yet"
+        NO_REVIEW_FOUND:"There's no review found"
     },
     ar: {
         LOGOUT_SUCCESS:"تم تسجيل الخروج بنجاح",
@@ -171,7 +171,7 @@ export const allMessages =
         CODE_DURATION_NOT_ENDED:"!لا يمكنك طلب رمز جديد ، حتى 5 دقائق من آخر رمز مرسل",
         EMAIL_NOT_FOUND:"غير مسجل... الرجاء التسجيل",
         EMAIL_SAME_AS_OLD:"هذا هو بريدك الإلكتروني القديم ، يجب عليك اختيار بريد إلكتروني مختلف",
-        NO_REVIEW_FOUND:"لا يوجد مراجعة لهذا الطبيب حتى الآن",
+        NO_REVIEW_FOUND:"لم يتم العثور علي تقييم",
 
 
     }
