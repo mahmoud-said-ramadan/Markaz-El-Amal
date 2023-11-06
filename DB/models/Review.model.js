439e50d40cf0a77cd8e643ae69c857c1853e6e96
yousef <yousefmahmoudmahmed@outlook.com>
2023-11-06 20:02:09 +0200

    remove default value for rate

--- DB/models/Review.model.js
+++ DB/models/Review.model.js
@@ -3,7 +3,7 @@ import mongoose, { Schema, Types, model } from "mongoose";
 const reviewSchema = new Schema(
   {
     comment: String,
-    rate: { type: Number, default: 0, required: true },
+    rate: { type: Number, required: true },
     patientId: { type: Types.ObjectId, ref: "Patient", required: true },
     doctorId: { type: Types.ObjectId, ref: "Doctor", required: true },
   },
