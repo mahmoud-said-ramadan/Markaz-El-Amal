f0fca602dfcc47703dc3211bb444a721e8e45ef8
yousef <yousefmahmoudmahmed@outlook.com>
2023-11-07 02:41:36 +0200

    create update date endpoint

--- src/modules/doctor/controller/updateDate.js
+++ src/modules/doctor/controller/updateDate.js
@@ -7,8 +7,9 @@ import { allMessages } from "../../../utils/localizationHelper.js";
  * Who authorized => doctor
  */
 const updateDate = asyncErrorHandler(async (req, res, next) => {
+  const time = new Date(req.body.time);
   for (const appointment of req.user.appointment) {
-    if (appointment.time == req.body.time) {
+    if (appointment.time.getTime() == time.getTime()) {
       if (req.body.from) appointment.from = req.body.from;
       if (req.body.to) appointment.to = req.body.to;
       break;
