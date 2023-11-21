81b9b1ec43a69629644845d8afbb36a4e9722191
yousef <yousefmahmoudmahmed@outlook.com>
2023-11-22 01:41:05 +0200

    apply validation

--- src/modules/review/review.router.js
+++ src/modules/review/review.router.js
@@ -5,9 +5,10 @@ import getReview from "./controller/getReview.js";
 import addReview from "./controller/addReview.js";
 import updateReview from "./controller/updateReview.js";
 import deleteReview from "./controller/deleteReview.js";
+import * as validator from "./review.validation.js";
+import { validateId, validation } from "../../middleware/validation.js";
 
 const router = Router();
-
 // addReview
 // updateReview
 // deleteReview
@@ -15,9 +16,13 @@ const router = Router();
 
 router
   .route(["/", "/:id"])
-  .post(auth(reviewEndpoint.add_update), addReview)
-  .put(auth(reviewEndpoint.add_update), updateReview)
-  .delete(auth(reviewEndpoint.get_delete), deleteReview)
-  .get(auth(reviewEndpoint.get_delete), getReview);
+  .post(auth(reviewEndpoint.add_update), validation(validator.add), addReview)
+  .put(
+    auth(reviewEndpoint.add_update),
+    validation(validator.update),
+    updateReview
+  )
+  .delete(auth(reviewEndpoint.get_delete), validation(validateId), deleteReview)
+  .get(auth(reviewEndpoint.get_delete), validation(validator.get), getReview);
 
 export default router;
