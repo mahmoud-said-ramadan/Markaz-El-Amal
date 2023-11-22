c05ae7baf7cc39b462726147ea37ada40d180a17
yousef <yousefmahmoudmahmed@outlook.com>
2023-11-22 18:28:09 +0200

    apply validation on common model

--- src/modules/common/common.router.js
+++ src/modules/common/common.router.js
@@ -8,33 +8,42 @@ import changeEmailController from "./controller/changeEmail.js";
 import logoutController from "./controller/logout.js";
 import updatePersonalInfoController from "./controller/updatePersonalInfo.js";
 import getUserController from "./controller/getUser.js";
-
+import { validateId, validation } from "../../middleware/validation.js";
+import * as validator from "./common.validation.js";
 const router = Router();
 
 // .route("/:id")
 router
   .route(["/", "/:id"])
-  .get(auth(commonEndPoint.getUser), getUserController)
+  // .get(auth(commonEndPoint.getUser), validation(validateId), getUserController)
   .put(
     auth(commonEndPoint.update),
-    fileUpload(filesValidation.image).single("image"),
+    // fileUpload(filesValidation.image).single("image"),
     updatePersonalInfoController
   );
 router.patch(
   "/changePassword",
   auth(commonEndPoint.update),
+  // validation(validator.changePassword),
   changePasswordController
 );
 router.patch(
   "/changeEmail",
   auth(commonEndPoint.update),
+  // validation(validator.changeEmail),
   changeEmailController
 );
 router.patch(
   "/confirmChangeEmail",
   auth(commonEndPoint.update),
+  // validation(validator.confirmChangeEmail),
   confirmChangeEmailController
 );
-router.patch("/logout", auth(commonEndPoint.update), logoutController);
+router.patch(
+  "/logout",
+  auth(commonEndPoint.update),
+  // validation(validator.logout),
+  logoutController
+);
 
 export default router;
