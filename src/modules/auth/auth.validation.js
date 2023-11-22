3acef55d55ccb15c8584ec7373fd88bda066e9ca
yousef <yousefmahmoudmahmed@outlook.com>
2023-11-22 02:56:29 +0200

    edit auth validation file

--- src/modules/auth/auth.validation.js
+++ src/modules/auth/auth.validation.js
@@ -1,31 +1,115 @@
 import joi from "joi";
 import { generalFields } from "../../middleware/validation.js";
 
-export const signup = joi.object({
-    name: joi.string().trim().min(3).max(25).required(),
-    email: generalFields.email.lowercase().required(),
-    // will make a password pattern later
-    password: generalFields.password.min(8).required(),
-    phone: joi.string().trim().pattern(/^\+?[1-9]\d{1,11}$/),
-    file: generalFields.file,
-}).required()
+export const signup = {
+  body: joi
+    .object({
+      name: joi.string().trim().min(3).max(25).required(),
+      email: generalFields.email.lowercase().required(),
+      // will make a password pattern later
+      password: generalFields.password.required(),
+      cPassword: joi.string().valid(joi.ref("password")).required(),
+      phone: joi
+        .string()
+        .trim()
+        .pattern(/^01[0-2,5]{1}[0-9]{8}$/),
+      OTP: joi.string().length(5).required(),
+      hashOTP: joi.string().required(),
+    })
+    .required(),
+  params: joi
+    .object({
+      role: joi.string().valid("patient", "doctor", "admin").required(),
+    })
+    .required(),
+  query: joi
+    .object({
+      ln: generalFields.ln.required(),
+    })
+    .required(),
+  file: generalFields.file,
+};
 
-export const login = joi.object({
-    email: generalFields.email.lowercase().required(),
-    password: joi.string().min(8).required(),
-}).required()
+export const login = {
+  body: joi
+    .object({
+      email: generalFields.email.lowercase().required(),
+      password: generalFields.password.required(),
+    })
+    .required(),
+  params: joi
+    .object({
+      role: joi.string().valid("patient", "doctor", "admin").required(),
+    })
+    .required(),
+  query: joi
+    .object({
+      ln: generalFields.ln.required(),
+    })
+    .required(),
+};
 
-export const confirm = joi.object({
-    email: generalFields.email.lowercase().required(),
-    code: joi.string().length(6).required(),
-}).required()
+export const confirm = {
+  body: joi
+    .object({
+      email: generalFields.email.lowercase().required(),
+      code: joi
+        .string()
+        .length(5)
+        .regex(/^[0-9]{5}$/)
+        .required(),
+    })
+    .required(),
+  params: joi
+    .object({
+      role: joi.string().valid("patient", "doctor", "admin").required(),
+    })
+    .required(),
+  query: joi
+    .object({
+      ln: generalFields.ln.required(),
+    })
+    .required(),
+};
 
-export const reset = joi.object({
-    newPassword: generalFields.password.min(8).required(),
-    email: generalFields.email.lowercase().required(),
-    code: joi.string().length(6).required(),
-}).required()
+export const reset = {
+  body: joi
+    .object({
+      newPassword: generalFields.password.required(),
+      email: generalFields.email.lowercase().required(),
+      OTP: joi
+        .string()
+        .length(5)
+        .regex(/^[0-9]{5}$/)
+        .required(),
+    })
+    .required(),
+  params: joi
+    .object({
+      role: joi.string().valid("patient", "doctor", "admin").required(),
+    })
+    .required(),
+  query: joi
+    .object({
+      ln: generalFields.ln.required(),
+    })
+    .required(),
+};
 
-export const generate = joi.object({
-    email: generalFields.email.lowercase().required(),
-}).required()
+export const generate = {
+  body: joi
+    .object({
+      email: generalFields.email.lowercase().required(),
+    })
+    .required(),
+  params: joi
+    .object({
+      role: joi.string().valid("patient", "doctor", "admin").required(),
+    })
+    .required(),
+  query: joi
+    .object({
+      ln: generalFields.ln.required(),
+    })
+    .required(),
+};
