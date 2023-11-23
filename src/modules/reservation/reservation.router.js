e2ed77363d2b97a5c15b5afaa7726e7be6bd52be
NadaAmrr <nada.amr1267@gmail.com>
2023-11-23 18:03:14 +0200

    reservation router

--- src/modules/reservation/reservation.router.js
+++ src/modules/reservation/reservation.router.js
@@ -3,27 +3,40 @@ import { auth } from "../../middleware/auth.js";
 import reservationEndpoint from "./reservation.endPoint.js";
 import makeReservation from "./controller/makeReservation.js";
 import confirmReservation from "./controller/confirmReservation.js";
-import cancelReservation from "./controller/cancelReservation.js";
+import {
+  cancelReservationDoctor,
+  cancelReservationPatient,
+} from "./controller/cancelReservation.js";
 import reservationNeedConfirm from "./controller/getNeedConfirmReservation.js";
 const router = Router();
+// Make reservation (patient)
 router.patch(
   "/patient/:reservationId",
   auth(reservationEndpoint.makeReservation),
   makeReservation
 );
+// cancel reservation (doctor)
 router.patch(
   "/doctor/:reservationId/cancel",
-  auth(reservationEndpoint.cancelReservation),
-  cancelReservation
+  auth(reservationEndpoint.cancelReservationDoctor),
+  cancelReservationDoctor
 );
+// cancel reservation (patient)
 router.patch(
-    "/doctor/:reservationId/confirm",
-    auth(reservationEndpoint.confirmReservation),
-    confirmReservation
-  );
+  "/patient/:reservationId/cancel",
+  auth(reservationEndpoint.cancelReservationPatient),
+  cancelReservationPatient
+);
+// confirm reservation (doctor)
+router.patch(
+  "/doctor/:reservationId/confirm",
+  auth(reservationEndpoint.confirmReservation),
+  confirmReservation
+);
+// Get need confirm reservations (doctor)
 router.get(
-    "/doctor/",
-    auth(reservationEndpoint.reservationNeedConfirm),
-    reservationNeedConfirm
-  );
+  "/doctor/",
+  auth(reservationEndpoint.reservationNeedConfirm),
+  reservationNeedConfirm
+);
 export default router;
