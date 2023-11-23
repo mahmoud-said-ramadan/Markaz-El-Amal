9f83d1818ac0f8e7004e9e8c4bb300cd406e86f3
NadaAmrr <nada.amr1267@gmail.com>
2023-11-23 18:04:12 +0200

    reservation endpoint

--- src/modules/reservation/reservation.endPoint.js
+++ src/modules/reservation/reservation.endPoint.js
@@ -3,7 +3,8 @@ import { Roles } from "../../middleware/auth.js";
 const reservationEndpoint = {
     makeReservation: [Roles.user],
     confirmReservation: [Roles.doctor],
-    cancelReservation: [Roles.doctor],
+    cancelReservationDoctor: [Roles.doctor],
+    cancelReservationPatient: [Roles.user],
     reservationNeedConfirm: [Roles.doctor]
 };
 export default reservationEndpoint;
