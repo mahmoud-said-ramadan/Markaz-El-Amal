b2280d83fea84baacefff4470898a5d313e41231
yousef <yousefmahmoudmahmed@outlook.com>
2023-11-26 00:15:17 +0200

    make changes in validation auth

--- src/modules/auth/auth.validation.js
+++ src/modules/auth/auth.validation.js
@@ -1,7 +1,14 @@
 import joi from "joi";
 import { generalFields } from "../../middleware/validation.js";
 
-export const signup = {
+export const signup = (req) => {
+  if (req.originalUrl.includes("/patient")) {
+    return signupPatient;
+  }
+  return signupDoctor;
+};
+
+const signupPatient = {
   body: joi
     .object({
       name: joi.string().trim().min(3).max(25).required(),
@@ -12,6 +19,35 @@ export const signup = {
       phone: generalFields.phone.required(),
       OTP: generalFields.code.required(),
       hashOTP: joi.string().required(),
+    })
+    .required(),
+  params: joi
+    .object({
+      role: joi.string().valid("patient", "doctor", "admin").required(),
+    })
+    .required(),
+  query: joi
+    .object({
+      ln: generalFields.ln.required(),
+    })
+    .required(),
+  file: generalFields.file,
+};
+
+const signupDoctor = {
+  body: joi
+    .object({
+      name: joi.string().trim().min(3).max(25).required(),
+      email: generalFields.email.lowercase().required(),
+      // will make a password pattern later
+      password: generalFields.password.required(),
+      cPassword: joi.string().valid(joi.ref("password")).required(),
+      phone: generalFields.phone.required(),
+      OTP: generalFields.code.required(),
+      token: joi.string().required(),
+      bio:joi.string().min(3).required(),
+      duration:joi.number().integer().positive().min(10).required(),
+    
     })
     .required(),
   params: joi
